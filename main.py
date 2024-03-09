6f5a82666bfbbae0e6ac04ecd0b572a5b353dac2
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-09 14:27:00 +0530

    Add imports and initialize screen

---
+++ main.py
@@ -0,0 +1,9 @@
+import time
+from turtle import Screen
+from car_manager import CarManager
+from scoreboard import Scoreboard
+
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.tracer(0)
