f6339e16c2f5e4ff05b1aaac9893b4ff3b576dbc
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-12 06:05:32 +0530

    Add car manager functionality

--- main.py
+++ main.py
@@ -14,8 +14,12 @@ player = Player()
 screen.listen()
 screen.onkey(fun=player.go_up, key="Up")
 
+can_manager = CarManager()
 
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    can_manager.create_car()
+    can_manager.move_cars()
