7b64d2affa4773b018a2775c2752d7b27948473e
Pratham Agarwal <prathamagarwal815@gmail.com>
2024-03-12 05:41:57 +0530

    Add player control functionality to the game

--- main.py
+++ main.py
@@ -2,11 +2,16 @@ import time
 from turtle import Screen
 from car_manager import CarManager
 from scoreboard import Scoreboard
+from player import Player
 
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+screen.listen()
+screen.onkey(fun=Player.go_up, key="Up")
+
 
 game_is_on = True
 while game_is_on:
